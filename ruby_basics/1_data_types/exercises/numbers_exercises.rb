93a4c738b9b27e41b172a0888b53799f2955c6a5
SaiyuktaR <102798914+SaiyuktaR@users.noreply.github.com>
2023-02-08 18:44:20 +0000

    feat: mutiply green

--- ruby_basics/1_data_types/exercises/numbers_exercises.rb
+++ ruby_basics/1_data_types/exercises/numbers_exercises.rb
@@ -10,12 +10,13 @@ end
 
 def multiply(a, b)
   # return the result of multiplying a times b
+  a * b
 end
 
 def divide(a, b)
   # return the result of dividing a by b
 end
-sai
+
 def remainder(a, b)
   # return the remainder of dividing a by b using the modulo operator
 end

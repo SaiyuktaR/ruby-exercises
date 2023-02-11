93a66a88c5635120b3b5bb1fe53d4cc8988c2188
SaiyuktaR <102798914+SaiyuktaR@users.noreply.github.com>
2023-02-11 16:16:02 +0000

    feat: array reverse green

--- ruby_basics/6_arrays/spec/array_exercises_spec.rb
+++ ruby_basics/6_arrays/spec/array_exercises_spec.rb
@@ -117,7 +117,7 @@ RSpec.describe 'Array Exercises' do
 
   describe 'reverse exercise' do
 
-    xit 'returns an array containing the elements in reverse order' do
+    it 'returns an array containing the elements in reverse order' do
       expect(reverse([0, 1, 1, 2, 3, 5])).to eq([5, 3, 2, 1, 1, 0])
     end
 

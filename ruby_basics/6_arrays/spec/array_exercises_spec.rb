b1d90c03d2c2e3945b5c776dc4f57941e420d068
SaiyuktaR <102798914+SaiyuktaR@users.noreply.github.com>
2023-02-11 16:04:17 +0000

    fix: array adding two array of numbers green

--- ruby_basics/6_arrays/spec/array_exercises_spec.rb
+++ ruby_basics/6_arrays/spec/array_exercises_spec.rb
@@ -84,7 +84,7 @@ RSpec.describe 'Array Exercises' do
 
   describe 'array concatenation exercise' do
 
-    xit 'returns an array adding two arrays of numbers together' do
+    it 'returns an array adding two arrays of numbers together' do
       expect(array_concatenation([1, 3, 5], [2, 4, 6])).to eq([1, 3, 5, 2, 4, 6])
     end
 
